6aadad4127cc0cccc1bf9af9eda5c037b47578a0
Agustin cilli <cilli.agustin@gmail.com>
2022-06-02 10:39:50 +0000

    created contact form for 90s version

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ let iconModern = document.getElementById("icon-modern");
 let iconFast = document.getElementById("icon-fast");
 let minimalCoffee = document.getElementById("minimal-coffee");
 let contador = document.getElementById("contador-calorias");
+let contactForm = document.getElementById("contact-form");
 let footer = document.getElementById("footer");
 
 eighties.addEventListener(("click"), setEighties);
@@ -155,7 +156,7 @@ function setEighties() {
         <a href="https://github.com/cilliagustin/ContadorDeCalorias/" class="btn-custom" target="_blank">Visit website</a>
         <a href="https://cilliagustin.github.io/ContadorDeCalorias/" target="_blank"class="btn-custom">Github repository</a>
 </div>`;
-    footer.innerHTML= `<p>© 2022 Agustin Cilli</p>
+footer.innerHTML= `<p>© 2022 Agustin Cilli</p>
     <div class="footer-icons">
         <a href="https://github.com/cilliagustin?tab=repositories" target="_blank" class="btn-custom">Visit Github</a>
         <a href = "mailto:abc@example.com?subject=Contact information" target="_blank" class="btn-custom">Send Email</a>
@@ -178,6 +179,56 @@ iconModern.innerHTML= `<div class="icon-container">
 iconFast.innerHTML= `<div class="icon-container">
     <img src="assets/images/mouse-speed90.png" alt="">
 </div>`;
+minimalCoffee.innerHTML= `<h4 class="portfolio-title">Minimal Coffee</h4>
+<div class="portfolio-content">
+    <p class="portfolio-text">A website for a fake coffee franchise located in Argentina.<br>The website is divided in 5 pages: <span class="highlight">Inicio</span> (Home), <span class="highlight">Nosotros</span> (About us), <span class="highlight">Menu</span>, <span class="highlight">Sucursales</span> (Locations), <span class="highlight">Contacto</span> (Contact). <br>The goal of this website is to show a correct use of the <span class="highlight">SEO</span> and <span class="highlight">front end tools</span>.</p>
+    <hr>
+    <div class="portfolio-bottom-info flex-start">
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>Bootstrap</p>
+        <p>Lang: SP</p>
+    </div>
+</div>
+<div class="portfolio-links flex-between">
+    <a href="https://cilliagustin.github.io/minimalcoffee/" class="btn-custom" target="_blank">Visit website</a>
+    <a href="https://github.com/cilliagustin/minimalcoffee" class="btn-custom" target="_blank">Github Repository</a>
+</div>`;
+contador.innerHTML= `<h4 class="portfolio-title">Contador de calorías</h4>
+<div class="portfolio-content">
+    <p class="portfolio-text"><span class="highlight">Single page web app</span> made to keep record of the food you eat as well of the calories ingested. This website uses the <span class="highlight">materialize  framework</span> to provide the grid.<br>The website <span class="highlight">stores data in the browser</span> to keep control over the days.</p>
+    <hr>
+    <div class="portfolio-bottom-info flex-start">
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>Lang: SP</p>
+    </div>
+</div>
+<div class="portfolio-links flex-between">
+    <a href="https://github.com/cilliagustin/ContadorDeCalorias/" class="btn-custom" target="_blank">Visit website</a>
+    <a href="https://cilliagustin.github.io/ContadorDeCalorias/" class="btn-custom" target="_blank">Github Repository</a>
+</div>`
+contactForm.innerHTML=` <h3>contact form</h3>
+<div class="contact-info">
+    <div class="contact-image">
+        <img src="assets/images/computers90.png" alt="">
+        <img src="assets/images/mail90.png" alt="">
+        <img src="assets/images/mail90.png" alt="">
+        <img src="assets/images/phone90.png" alt="">
+        <img src="assets/images/msn90.png" alt="">
+    </div>
+    <form action="" method="post">
+        <label for="fullname">Full name</label>
+        <input type="text" id="fullname" name="fullname" placeholder="Write your full name please">
+        <label for="email">Email address</label>
+        <input type="email" id="email" name="email" placeholder="Write your email address please">
+        <label for="comments">Message</label>
+        <textarea name="comments" id="comments" cols="30" rows="10" placeholder="Anything else I should know?..."></textarea>
+    </form>
+</div>
+<hr>
+<button class="btn-custom">Send</button>`;
 }
 
 function setPresent() {
