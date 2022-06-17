e13c9fb9387c426f8e70525377413b63f70378cd
Agustin cilli <cilli.agustin@gmail.com>
2022-06-17 16:22:42 +0000

    add function to switch active class in navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-//common variables
+//general variables
 let cssFile = document.getElementById("style");
 let favicon = document.getElementById("favicon");
 let loader = document.getElementById("loader");
@@ -8,7 +8,9 @@ let twoThousands = document.getElementById("00s");
 let twentyTen = document.getElementById("10s");
 let twentyTwenty = document.getElementById("20s");
 let logo = document.getElementById("logo");
+let navLinks = document.querySelectorAll("#menu ul li");
 let iconLink = Array.from(document.querySelectorAll("[icon-link]"));
+let sections = document.querySelectorAll("[site-section]");
 let btnLink = Array.from(document.querySelectorAll("[btn-link]"));
 let portInfoIcon = Array.from(document.querySelectorAll("[portfolio-info-icon]"));
 let portInfoText = Array.from(document.querySelectorAll("[portfolio-info-text]"));
@@ -45,7 +47,6 @@ let twentyTenIconNavegate = document.getElementById("2010iconNavegate");
 let twentyTenIconModern = document.getElementById("2010iconModern");
 let twentyTenIconFast = document.getElementById("2010iconFast");
 
-
 //2020 variables
 let twentyTwentyAboutImg = document.getElementById("2020aboutImg");
 let twentyTwentyAboutBorder = document.getElementById("2020aboutBorder");
@@ -54,105 +55,146 @@ let twentyTwentyIconNavegate = document.getElementById("2020iconNavegate");
 let twentyTwentyIconModern = document.getElementById("2020iconModern");
 let twentyTwentyIconFast = document.getElementById("2020iconFast");
 
-
-
-
+//CHANGE WEBSITE FUNCTIONS
 eighties.addEventListener(("click"), setEighties);
 nineties.addEventListener(("click"), setNineties);
 twoThousands.addEventListener(("click"), setTwoThousands);
 twentyTen.addEventListener(("click"), setTwentyTen);
 twentyTwenty.addEventListener(("click"), setTwentyTwenty);
 
-function setEighties() {
-
-    let eightiesElements = [eightiesAboutImg, eightiesIconResponsive, eightiesIconNavegate, eightiesIconModern, eightiesIconFast, btnLink, portInfoText];
-    cssFile.setAttribute("href", "assets/css/style80.css");
-    logo.setAttribute("src", "assets/images/2020logowhite.png");
-    hideAll();
-    eightiesElementsFlat = eightiesElements.flat();
-    eightiesElementsFlat.forEach(function(el) {
-        el.classList.remove("hide")
+//hides all elements
+function hideAll(){
+    let allElements = [
+        eightiesAboutImg, eightiesIconResponsive, eightiesIconNavegate, 
+        eightiesIconModern, eightiesIconFast, ninetiesAboutImg, 
+        ninetiesIconResponsive, ninetiesIconNavegate, ninetiesIconModern, 
+        ninetiesIconFast, ninetiesContactImg, ninetiesContactTitle, ninetiesHr, 
+        twoThousandsAboutImg, twoThousandsIconResponsive, 
+        twoThousandsIconNavegate, twoThousandsIconModern, twoThousandsIconFast, 
+        twentyTenProfilePic, twentyTenAboutImg, twentyTenIconResponsive, 
+        twentyTenIconNavegate, twentyTenIconModern, twentyTenIconFast, 
+        twentyTwentyAboutImg, twentyTwentyAboutBorder, twentyTwentyIconResponsive, 
+        twentyTwentyIconNavegate, twentyTwentyIconModern, twentyTwentyIconFast, 
+        iconLink, btnLink, portInfoIcon, portInfoText].flat();
+    
+    allElements.forEach(function(el) {
+        el.classList.add("hide")
     });
-}
 
-function setNineties() {
-
-    let ninetiesElements = [ninetiesAboutImg, ninetiesIconResponsive, ninetiesIconNavegate, ninetiesIconModern, ninetiesIconFast, btnLink, portInfoText,ninetiesContactTitle, ninetiesContactImg, ninetiesHr];
-    cssFile.setAttribute("href", "assets/css/style90.css");
-    favicon.setAttribute("href", "assets/images/1990favicon.png");
-    logo.setAttribute("src", "assets/images/1990logo.png");
-    hideAll();
-    ninetiesElementsFlat = ninetiesElements.flat();
-    ninetiesElementsFlat.forEach(function(el) {
-        el.classList.remove("hide")
-    });
-}
-
-function setTwoThousands() {
-
-    let twoThousandsElements = [twoThousandsAboutImg, twoThousandsIconResponsive, twoThousandsIconNavegate, twoThousandsIconModern, twoThousandsIconFast,
-    btnLink, portInfoText];
-    cssFile.setAttribute("href", "assets/css/style00.css");
-    favicon.setAttribute("href", "assets/images/2000favicon.png");
-    logo.setAttribute("src", "assets/images/2000logo.png");
-    hideAll();
-    twoThousandsElementsFlat = twoThousandsElements.flat();
-    twoThousandsElementsFlat.forEach(function(el) {
-        el.classList.remove("hide")
-    });
-}
-
-function setTwentyTen() {
-
-    let twentyTenElements = [twentyTenProfilePic, twentyTenAboutImg, twentyTenIconResponsive, twentyTenIconNavegate, twentyTenIconModern, twentyTenIconFast, iconLink, portInfoIcon]
-    cssFile.setAttribute("href", "assets/css/style10.css");
-    favicon.setAttribute("href", "assets/images/2010logo.png");
-    logo.setAttribute("src", "assets/images/2010logo.png");
-    hideAll();
-    twentyTenElementsFlat = twentyTenElements.flat();
-    twentyTenElementsFlat.forEach(function(el) {
-        el.classList.remove("hide")
-    });
-}
-
-function setTwentyTwenty() {
-
-    let TwentyTwentyElements = [twentyTwentyAboutImg, twentyTwentyAboutBorder, twentyTwentyIconResponsive, twentyTwentyIconNavegate, twentyTwentyIconModern, twentyTwentyIconFast, iconLink, portInfoIcon];
-    cssFile.setAttribute("href", "assets/css/style20.css");
-    favicon.setAttribute("href", "assets/images/2020favicon.png");
-    logo.setAttribute("src", "assets/images/2020logo.png");
-    hideAll();
-    TwentyTwentyElementsFlat = TwentyTwentyElements.flat();
-    TwentyTwentyElementsFlat.forEach(function(el) {
-        el.classList.remove("hide")
-    });
 }
 
 //loader functions
 function displayLoader(){
     loader.classList.remove("hide");
+    document.body.style.overflowY = "hidden";
 };
 
+function blockLoader(){
+    setTimeout(() => {
+        loader.classList.add("hide");
+        document.body.style.overflowY = "auto";
+    }, 1500);
+};
 
-//hides all elements
-function hideAll(){
-    let allElements = [
-        eightiesAboutImg, eightiesIconResponsive, eightiesIconNavegate, eightiesIconModern, eightiesIconFast, 
-    
-        ninetiesAboutImg, ninetiesIconResponsive, ninetiesIconNavegate, ninetiesIconModern, ninetiesIconFast, ninetiesContactImg, ninetiesContactTitle, ninetiesHr,
+//set decades functions
 
-        twoThousandsAboutImg, twoThousandsIconResponsive, twoThousandsIconNavegate, twoThousandsIconModern, twoThousandsIconFast,
-
-        twentyTenProfilePic, twentyTenAboutImg, twentyTenIconResponsive, twentyTenIconNavegate, twentyTenIconModern, twentyTenIconFast,
-        
-        twentyTwentyAboutImg, twentyTwentyAboutBorder, twentyTwentyIconResponsive, twentyTwentyIconNavegate, twentyTwentyIconModern, twentyTwentyIconFast,
-    
-        iconLink, btnLink, portInfoIcon, portInfoText
-    ];
-    
-    allElementsFlat = allElements.flat();
-    allElementsFlat.forEach(function(el) {
-        el.classList.add("hide")
+function setEighties() {
+    displayLoader();
+    cssFile.setAttribute("href", "assets/css/style80.css");
+    favicon.setAttribute("href", "assets/images/1980favicon.png");
+    logo.setAttribute("src", "assets/images/1980logo.png");
+    hideAll();
+    let eightiesElements = [eightiesAboutImg, eightiesIconResponsive, 
+        eightiesIconNavegate, eightiesIconModern, eightiesIconFast, 
+        btnLink, portInfoText].flat();
+    eightiesElements.forEach(function(el) {
+        el.classList.remove("hide")
     });
-
+    blockLoader();
 }
+
+function setNineties() {
+    displayLoader();
+    cssFile.setAttribute("href", "assets/css/style90.css");
+    favicon.setAttribute("href", "assets/images/1990favicon.png");
+    logo.setAttribute("src", "assets/images/1990logo.png");
+    hideAll();
+    let ninetiesElements = [ninetiesAboutImg, ninetiesIconResponsive, 
+        ninetiesIconNavegate, ninetiesIconModern, ninetiesIconFast, 
+        btnLink, portInfoText,ninetiesContactTitle, ninetiesContactImg, 
+        ninetiesHr].flat();
+    ninetiesElements.forEach(function(el) {
+        el.classList.remove("hide")
+    });
+    blockLoader();
+}
+
+function setTwoThousands() {
+    displayLoader();
+    cssFile.setAttribute("href", "assets/css/style00.css");
+    favicon.setAttribute("href", "assets/images/2000favicon.png");
+    logo.setAttribute("src", "assets/images/2000logo.png");
+    hideAll();
+    let twoThousandsElements = [twoThousandsAboutImg, twoThousandsIconResponsive, 
+        twoThousandsIconNavegate, twoThousandsIconModern, twoThousandsIconFast,
+        btnLink, portInfoText].flat();
+    twoThousandsElements.forEach(function(el) {
+        el.classList.remove("hide")
+    });
+    blockLoader();
+}
+
+function setTwentyTen() {
+    displayLoader();
+    cssFile.setAttribute("href", "assets/css/style10.css");
+    favicon.setAttribute("href", "assets/images/2010logo.png");
+    logo.setAttribute("src", "assets/images/2010logo.png");
+    hideAll();
+    let twentyTenElements = [twentyTenProfilePic, twentyTenAboutImg, 
+        twentyTenIconResponsive, twentyTenIconNavegate, twentyTenIconModern, 
+        twentyTenIconFast, iconLink, portInfoIcon].flat()
+    twentyTenElements.forEach(function(el) {
+        el.classList.remove("hide")
+    });
+    blockLoader();
+}
+
+function setTwentyTwenty() {
+    displayLoader();
+    cssFile.setAttribute("href", "assets/css/style20.css");
+    favicon.setAttribute("href", "assets/images/2020favicon.png");
+    logo.setAttribute("src", "assets/images/2020logo.png");
+    hideAll();
+    let TwentyTwentyElements = [twentyTwentyAboutImg, twentyTwentyAboutBorder, 
+        twentyTwentyIconResponsive, twentyTwentyIconNavegate, 
+        twentyTwentyIconModern, twentyTwentyIconFast, iconLink, portInfoIcon].flat();
+    TwentyTwentyElements.forEach(function(el) {
+        el.classList.remove("hide")
+    });
+    blockLoader();
+}
+
+// ADD ACTIVE CLASS WHILE SCROLL
+
+
+window.addEventListener("scroll", () => {
+    var current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+          current = section.getAttribute("id"); }
+      });
+    
+      navLinks.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+          li.classList.add("active");
+        }
+      });
+  });
+
+
+
+
+
+
