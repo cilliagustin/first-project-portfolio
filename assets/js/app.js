60c05b5ad5c964f297ad938cc4738983a6a6f0b7
Agustin cilli <cilli.agustin@gmail.com>
2022-06-24 15:55:36 +0000

    replace pageYoffset for window.scrolly on js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -183,7 +183,7 @@ window.addEventListener("scroll", () => {
     let current = "";
     sections.forEach((section) => {
         const sectionTop = section.offsetTop;
-        if (pageYOffset >= sectionTop - 60) {
+        if (window.scrollY >= sectionTop - 60) {
           current = section.getAttribute("id"); }
       });
     
